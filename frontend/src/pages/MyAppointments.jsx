84a8081bb22e78cde7efca5ef925824a49a7c9a1
Piyush mishra <piyushmishra8982@gmail.com>
2026-02-18 13:46:43 +0530

    added my appointment

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,9 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
-
+import axios from 'axios';
+import {toast} from 'react-toastify'
 function MyAppointments() {
-  const { doctors } = useContext(AppContext);
+  const {  token, backendURL } = useContext(AppContext);
+const [appointments,setAppointments]=useState([]);
+const months=[" ","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+const slotDateFormat=(slotDate)=>{
+  const dateArray=slotDate.split('_');
+  return dateArray[0]+ " "+months[Number(dateArray[1])] + " " + dateArray[2];
 
+}
+const getUserAppointment=async ()=>{
+  try{
+     const {data}=await axios.get(backendURL +'/api/user/appointments',{headers:{token}});
+       if(data.success){
+        setAppointments(data.appointments.reverse());
+       }
+    }catch(error){
+    console.log(error);
+    toast.error(error.message);
+  }
+}
+useEffect(()=>{
+   if(token){
+    getUserAppointment();
+   }
+},[token]);
   return (
     <div className="px-6 md:px-12 py-10">
       <h3 className="text-2xl font-semibold mb-8 text-primary text-center">
@@ -11,32 +34,32 @@ function MyAppointments() {
       </h3>
 
       <div className="flex flex-col gap-6">
-        {doctors.slice(0, 2).map((item, index) => (
+        {appointments.map((item, index) => (
           <div
             key={index}
             className="flex flex-col md:flex-row gap-6 bg-white p-5 rounded-xl shadow-sm border hover:shadow-2xl hover:-translate-y-1 transition-all duration-300"
           >
             {/* Doctor Image */}
             <img
-              src={item.image}
+              src={item.docData.image}
               alt="doctor"
               className="w-32 h-32 object-cover rounded-lg"
             />
 
             {/* Info */}
             <div className="flex-1 text-sm text-gray-600">
-              <p className="text-lg font-semibold text-gray-800">{item.name}</p>
-              <p className="text-primary">{item.speciality}</p>
+              <p className="text-lg font-semibold text-gray-800">{item.docData.name}</p>
+              <p className="text-primary">{item.docData.speciality}</p>
 
               <div className="mt-2">
                 <p className="font-medium text-gray-700">Address:</p>
-                <p>{item.address.line1}</p>
-                <p>{item.address.line2}</p>
+                <p>{item.docData.address.line1}</p>
+                <p>{item.docData.address.line2}</p>
               </div>
 
               <p className="mt-2">
                 <span className="font-medium text-gray-700">Date & Time:</span>{" "}
-                10 July 2025 | 8:30 PM
+                {slotDateFormat(item.slotDate)} | {item.slotTime}
               </p>
             </div>
 
